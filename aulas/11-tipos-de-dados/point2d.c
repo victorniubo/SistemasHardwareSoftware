0082ec6c1074feb8789883bcea372f8ab75176bc
Igor Montagner <igordsm@gmail.com>
2020-10-15 15:46:15 -0300

    Deployed b8fcac6 with MkDocs version: 1.1.2

--- aulas/11-tipos-de-dados/point2d.c
+++ aulas/11-tipos-de-dados/point2d.c
@@ -9,31 +9,48 @@ typedef struct _p Point2D;
 
 
 Point2D *point2D_new(double x, double y) {
-    return NULL;
+    Point2D *ponto = malloc(sizeof(Point2D));
+    ponto->x = x;
+    ponto->y = y;
+    return ponto;
 }
 
 
 void point2D_destroy(Point2D *p) {
-    
+    free(p);
 }
 
 double point2D_get_x(Point2D *p) {
-    return 0;
+    return p->x; // (*p).x == p->x
 }
 
 double point2D_get_y(Point2D *p) {
-    return 0;
+    return p->y;
+}
+/*
+    double xn = point2D_get_x(p1)+point2D_get_x(p2);
+    double yn = point2D_get_y(p1)+point2D_get_y(p2);
+    Point2D *p3 = point2D_new(xn,yn);
+    return p3;
+*/
+Point2D *point2D_add(Point2D *p1, Point2D *p2) {
+    Point2D *p3 = point2D_new( p1->x+p2->x , p1->y+p2->y );
+    return p3;
 }
 
-Point2D *point2D_add(Point2D *p1, Point2D *p2) {
-    return NULL;
-}
+/*
+    double adjacente = point2D_get_x(p2) - point2D_get_x(p1);
+    double oposto = point2D_get_y(p2) - point2D_get_y(p1);
+    return oposto/adjacente;
+*/
 
 double point2D_theta(Point2D *p1, Point2D *p2) {
-    return 0;
+    double theta = (p2->y-p1->y)/(p2->x-p1->x);
+    return theta;
 }
 
 Point2D *point2D_scale(Point2D *p, double s) {
-    return NULL;
+    Point2D *ponto = point2D_new(point2D_get_x(p)*s,point2D_get_y(p)*s);
+    return ponto;
 }
 
--- aulas/11-tipos-de-dados/teste_ponto.c
+++ aulas/11-tipos-de-dados/teste_ponto.c
@@ -3,19 +3,19 @@
 
 int test_new_destroy_get() {
     Point2D *p = point2D_new(0, 0);
-    
+
     test_assert(point2D_get_x(p) == 0, "Erro na inicialização da componente x!");
     test_assert(point2D_get_y(p) == 0, "Erro na inicialização da componente y!");
-    
+
     point2D_destroy(p);
-    
+
     Point2D *p2 = point2D_new(10, 20);
-    
+
     test_assert(point2D_get_x(p2) == 10, "Erro na inicialização da componente x!");
     test_assert(point2D_get_y(p2) == 20, "Erro na inicialização da componente y!");
-    
+
     point2D_destroy(p2);
-    
+
     return 0;
 }
 
@@ -24,14 +24,14 @@ int test_add() {
     a = point2D_new(10.5, 20);
     b = point2D_new(30, 40.3);
     c = point2D_add(a, b);
-    
+
     test_assert(point2D_get_x(c) == 40.5, "Erro na soma: componente x!");
-    test_assert(point2D_get_y(c) == 60.3, "Erro na soma: componente x!");
-    
+    test_assert(point2D_get_y(c) == 60.3, "Erro na soma: componente y!");
+
     point2D_destroy(a);
     point2D_destroy(b);
     point2D_destroy(c);
-    
+
     return 0;
 }
 
@@ -39,19 +39,19 @@ int test_scale() {
     Point2D *a, *b, *c;
     a = point2D_new(10.5, 20);
     b = point2D_scale(a, 2.5);
-    
+
     test_assert(point2D_get_x(b) == 26.25, "Erro na escala: componente x!");
     test_assert(point2D_get_y(b) == 50, "Erro na escala: componente y!");
-    
+
     c = point2D_scale(a, 0);
-    
+
     test_assert(point2D_get_x(c) == 0, "Erro na escala: componente x!");
     test_assert(point2D_get_y(c) == 0, "Erro na escala: componente y!");
 
     point2D_destroy(a);
     point2D_destroy(b);
     point2D_destroy(c);
-    
+
     return 0;
 }
 
@@ -61,18 +61,18 @@ int test_theta() {
     b = point2D_new(40, 50);
 
     test_assert(point2D_theta(a, b) == 1, "Erro na inclinação da reta!");
-    
+
     point2D_destroy(a);
     point2D_destroy(b);
-    
+
     a = point2D_new(20, 30);
     b = point2D_new(40, 40);
 
     test_assert(point2D_theta(a, b) == 0.5, "Erro na inclinação da reta!");
-    
+
     point2D_destroy(a);
     point2D_destroy(b);
-    
+
     return 0;
 }
 
